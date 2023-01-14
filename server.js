64aedb2ec1cf9ff519dcd4222e4c7707efbc52ab
Fredi Anriko <fredianriko@gmail.com>
2023-01-14 20:05:27 +0700

    add comment for more understanding

--- server.js
+++ server.js
@@ -4,21 +4,29 @@ const app = express();
 
 app.get("/video", (req, res) => {
   const range = req.headers.range;
+  console.log("range", range);
 
   //path where the video is stored
   const videoPath = "./Laravel.mp4";
 
-  //size of the file
+  //size of the video file
   const videoSize = fs.statSync(videoPath).size;
+  console.log(fs.statSync(videoPath));
 
-  //define the chunk size
+  //define the chunk size to be 1 MB
   const chunkSize = 1 * 1e6;
 
+  //start of chunk buffer
   const start = range != undefined ? Number(range.replace(/\D/g, "")) : 0;
   console.log("START", start);
+
+  //end of chunk buffer
   const end = Math.min(start + chunkSize, videoSize - 1);
-  console.log("End", end);
+  console.log("END", end);
+
   const contentLength = end - start + 1;
+  console.log("CONTENT LENGTH", contentLength);
+  console.log("----------------------------------------");
 
   const headers = {
     "Content-Range": `bytes ${start}-${end}/${videoSize}`,
@@ -33,7 +41,9 @@ app.get("/video", (req, res) => {
   const stream = fs.createReadStream(videoPath, { start, end });
 
   //create socket between client and server, to keep alive connection
-  stream.pipe(res);
+  stream.pipe(res).on("error", (error) => {
+    console.log(error);
+  });
 });
 
 app.listen(3001);
